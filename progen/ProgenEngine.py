129f15da490e26bfcc76d1a68739d76314233853
Guillem <guillemavila@gmail.com>
2017-06-02 14:31:18 +0200

    Cura 2: Fixed combing

--- progen/ProgenEngine.py
+++ progen/ProgenEngine.py
@@ -1379,7 +1379,11 @@ def cura2Profile():
                     # qualityFile.append('jerk_travel_layer_0 = =jerk_layer_0 * jerk_travel / jerk_print')
                     # qualityFile.append('jerk_skirt_brim = =jerk_layer_0')
 
-                    # travel
+                    # travel                    
+                    if filament['isFlexibleMaterial']:
+                        qualityFile.append('retraction_combing = all')
+                    else:
+                        qualityFile.append('retraction_combing = noskin')
                     qualityFile.append('retraction_combing = all')
                     # qualityFile.append('travel_retract_before_outer_wall = False')
                     qualityFile.append('travel_avoid_other_parts = False')
